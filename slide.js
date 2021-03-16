75cef3782687878b2438f740369603664b558c7e
as3689 <as3689@naver.com>
2021-03-16 19:08:35 +0900

    자바스크립트 수정

---
+++ slide.js
@@ -0,0 +1,40 @@
+$(function () {
+  let $SeeIt = $ ("#seeit");
+  let $menu = $("#top_menu ul li");
+  let $contents = $("#contents > section");
+  
+  
+  
+  /*몇번쨰 이동햇는지 변수  */
+  $menu.click(function (event) {
+    event.preventDefault();
+    let idx = $(this).index();
+    let tt = $contents.eq(idx).offset().top;
+    console.log(tt);
+    $("html, body").animate({ scrollTop: tt });
+    
+    /*클릭한 부분만 색변환*/
+    $(this).addClass("active").siblings().removeClass("active");
+  });
+
+  $SeeIt.click(function (event) {
+    event.preventDefault();
+    let idx = $(1).index();
+    let tt = $contents.eq(1).offset().top;
+    console.log(tt);
+    $("html, body").animate({ scrollTop: tt });
+    $(this).addClass("active").siblings().removeClass("active");
+  });
+
+  /* 스크롤 이동시 반영될 부분 */
+  $(window).scroll(function () {
+    let $sct = $(this).scrollTop();
+    $contents.each(function (i) {
+      let taget = $(this);
+      if (taget.offset().top < $sct) {
+        $menu.removeClass("active");
+        $menu.eq(i).addClass("active");
+      }
+    });
+  });
+});
